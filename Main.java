02f4cbdb3017bfa1b85d414385f35cbd92eafe24
RiskaAml <amelriskaaa@gmail.com>
2025-06-11 14:46:10 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Device{
+    int year = 2025;
+    public void display(){
+        System.out.println("Displaying device info...");
+    }
+    static class Configuration{
+        public void setup(){
+            System.out.println("Setting up device configuration...");
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Device obj = new Device();
+        obj.display();
+        
+        Device.Configuration obj1 = new Device.Configuration();
+        // Karena Configuration adalah static inner class, bisa dibuat langsung lewat nama outer class
+        // Device.Configuraion obj1 = obj.new Configuration();  ini kalau B non static
+        obj1.setup();
+    }
+    
+}
